1182336019478b7b9802420020549c6f64087dd1
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-09-04 19:40:15 +0300

    Add dependancies and libraries for testing

--- app/src/main/java/com/moringaschool/motormax/MotorMaxActivity.java
+++ app/src/main/java/com/moringaschool/motormax/MotorMaxActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.motormax;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MotorMaxActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MotorMaxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motor_max);
+        Intent intent =getIntent();
+
     }
 }
