ae81de586d66d7b91503f0011f27f4ed9258d797
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-09-04 16:55:13 +0300

    Add new android version

---
+++ app/src/main/java/com/moringaschool/motormax/MotorMaxActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.motormax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotorMaxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motor_max);
+    }
+}
