515ff2168fb46f315953b150f98b88c3e08cdf2e
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-09-04 16:39:54 +0300

    Add onclick method to homepage button

--- app/src/main/java/com/moringaschool/motormax/MainActivity.java
+++ app/src/main/java/com/moringaschool/motormax/MainActivity.java
@@ -3,12 +3,26 @@ package com.moringaschool.motormax;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mDiscoverMotorButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDiscoverMotorButton =(Button)findViewById(R.id.discoverMotorButton);
+
+        mDiscoverMotorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "pash!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
