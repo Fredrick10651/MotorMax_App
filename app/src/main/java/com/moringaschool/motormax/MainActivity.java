0ba7583ce55dc7ccfabc9fe719d1734a42380679
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-09-04 22:55:44 +0300

    Created a list of car models in motormax activity class

--- app/src/main/java/com/moringaschool/motormax/MainActivity.java
+++ app/src/main/java/com/moringaschool/motormax/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mDiscoverMotorButton;
+    private TextView mLocationTextView;
 
 
 
--- app/src/main/java/com/moringaschool/motormax/MotorMaxActivity.java
+++ app/src/main/java/com/moringaschool/motormax/MotorMaxActivity.java
@@ -4,14 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MotorMaxActivity extends AppCompatActivity {
+    private ListView mListView;
+    private TextView mLocationTextView;
+    private final String [] motorModel = new String[] {"Toyota", "Ferrari", "Honda", "Ford", "BMW", "Chevrolet", "Volskswagen", "Audi", "Mercedes-Benz", "Hyundai Motor Company", "Mazda", "Mitsubishi", "Lexus", "Jeep" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motor_max);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, motorModel);
+        mListView.setAdapter(adapter);
+
         Intent intent =getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Car Models: " + location);
 
     }
 }
