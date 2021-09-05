bfc63e85eda4ca52a6a53780103756b6a3565473
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-09-05 10:36:19 +0300

    Add button to new view to enable selecting car models

--- app/src/main/java/com/moringaschool/motormax/MotorMaxActivity.java
+++ app/src/main/java/com/moringaschool/motormax/MotorMaxActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MotorMaxActivity extends AppCompatActivity {
+    private Button mSelectModeButton;
     private ListView mListView;
     private TextView mLocationTextView;
     private final String [] motorModel = new String[] {"Toyota", "Ferrari", "Honda", "Ford", "BMW", "Chevrolet", "Volskswagen", "Audi", "Mercedes-Benz", "Hyundai Motor Company", "Mazda", "Mitsubishi", "Lexus", "Jeep" };
@@ -17,6 +21,14 @@ public class MotorMaxActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motor_max);
+
+        mSelectModeButton =(Button)findViewById(R.id.selectModelButton);
+        mSelectModeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MotorMaxActivity.this, "PASH UKO", Toast.LENGTH_LONG).show();
+            }
+        });
 
         mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
