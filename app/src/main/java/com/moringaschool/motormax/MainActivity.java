bd6b2d2eae7f0d3dfd9b197bc569dd23b6247d02
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-09-06 22:07:37 +0300

    Refactoring codes with butterKnife method

--- app/src/main/java/com/moringaschool/motormax/MainActivity.java
+++ app/src/main/java/com/moringaschool/motormax/MainActivity.java
@@ -8,17 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mDiscoverMotorButton;
-    private TextView mLocationTextView;
+//    private Button mDiscoverMotorButton;
+//    private TextView mLocationTextView;
 
+    @BindView(R.id.discoverMotorButton) Button mDiscoverMotorButton;
+    @BindView(R.id.appNameTextView) TextView mLocationTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         mDiscoverMotorButton =(Button)findViewById(R.id.discoverMotorButton);
         mDiscoverMotorButton.setOnClickListener(new View.OnClickListener() {
 
