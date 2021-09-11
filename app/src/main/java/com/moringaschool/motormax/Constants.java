e797cff36dc9bf4633c55889c034d74bead34908
Fredrick10651 <Fredrick.mutsotso@student.moringaschool.com>
2021-09-11 11:00:59 +0300

    Create constant class and add base url and api information

--- app/src/main/java/com/moringaschool/motormax/Constants.java
+++ app/src/main/java/com/moringaschool/motormax/Constants.java
@@ -1,7 +1,10 @@
 package com.moringaschool.motormax;
 
+import android.os.Build;
+
 import java.security.Key;
 
 public class Constants {
-
+    public static final String XRapid_BASE_URL = "car-data.p.rapidapi.com";
+    public static final String X_RAPID_API_KEY = BuildConfig.X_RAPID_API_KEY;
 }
